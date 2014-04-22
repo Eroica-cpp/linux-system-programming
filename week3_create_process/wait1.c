b315931a84aa56308523f8e5fb22f530a3eefba9
Eroica-cpp <eroicacmcs@gmail.com>
2014-04-23 01:51:59 +0800

    update readme.md second time

--- week3_create_process/wait1.c
+++ week3_create_process/wait1.c
@@ -12,6 +12,20 @@ int main(){
 	if (pid == 0){
 		sleep(4);
 		exit(5);
+	} else {
+		printf("I am a parent process %d with child %d\n", getpid(), pid);
+	}
+
+	if ((pid = wait(&status)) == -1){
+		perror("wait failed\n");
+		exit(2);
+	}
+
+	// Q: what following code means? A: note p30
+	if (exit_status = WIFEXITED(status)){
+		printf("exit status from %d was %d, and the actual status was %d\n", pid, exit_status, WEXISTATUS(status));
 	} 
-	
+	exit(0);
+
+	return 0;
 }
