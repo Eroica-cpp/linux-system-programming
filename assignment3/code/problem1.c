e05cab74f58a5f4d4dc12eb6e1442cab9cf2c29e
Eroica-cpp <eroicacmcs@gmail.com>
2014-06-20 19:00:12 +0800

    update

--- assignment3/code/problem1.c
+++ assignment3/code/problem1.c
@@ -12,12 +12,22 @@ typedef struct
 void *threadfunc(void *parm)
 {
 	thread_parm_t *p = (thread_parm_t *) parm;
-	printf("%s, pram = %d\n", p -> string, p -> value);
+	printf("%s, parm = %d\n", p -> string, p -> value);
 	free(p);
 	return NULL;
 }
 
 int main(int argc, char **argv)
 {
+	pthread_t thread;
+	int rc = 0;
+	thread_parm_t *parm = NULL;
+	printf("Create a thread attributes objects\n");
+	parm = malloc(sizeof(thread_parm_t));
+	parm -> value = 5;
+	strcpy(parm -> string, "Inside first thread");
+	//rc = pthread_create(&thread, NULL, threadfunc, (void *) parm);
+	printf("%s, parm = %d\n", parm -> string, parm -> value);
+	printf("Main completed\n");
 	return 0;
 }
