a5524c7d0481dcb32ee9cb9300cdfc74f2d76e08
Eroica-cpp <eroicacmcs@gmail.com>
2014-06-20 19:37:42 +0800

    an ok version of fixed problem 1

--- assignment3/code/problem1.c
+++ assignment3/code/problem1.c
@@ -12,6 +12,11 @@ typedef struct
 void *threadfunc(void *parm)
 {
 	thread_parm_t *p = (thread_parm_t *) parm;
+	if (p -> value == 5) {
+		printf("thread 1 is sleeping\n");
+		sleep(2);
+		printf("thread 1 is awaked\n");
+	}
 	printf("%s, parm = %d\n", p -> string, p -> value);
 	free(p);
 	return NULL;
@@ -19,15 +24,26 @@ void *threadfunc(void *parm)
 
 int main(int argc, char **argv)
 {
-	pthread_t thread;
-	int rc = 0;
+	pthread_t thread1, thread2;
+	int rc = 0, status;
 	thread_parm_t *parm = NULL;
 	printf("Create a thread attributes objects\n");
+	
+	// first thread
 	parm = malloc(sizeof(thread_parm_t));
 	parm -> value = 5;
 	strcpy(parm -> string, "Inside first thread");
-	//rc = pthread_create(&thread, NULL, threadfunc, (void *) parm);
-	printf("%s, parm = %d\n", parm -> string, parm -> value);
+	rc = pthread_create(&thread1, NULL, threadfunc, (void *) parm);
+
+	// second thread
+	parm = malloc(sizeof(thread_parm_t));
+	parm -> value = 77;
+	strcpy(parm -> string, "Inside Second thread");
+	rc = pthread_create(&thread2, NULL, threadfunc, (void *) parm);
+	
+	pthread_join(thread1, (void *) &status);	// wait first thread
+	pthread_join(thread2, (void *) &status);	// wait second thread
+
 	printf("Main completed\n");
 	return 0;
 }
